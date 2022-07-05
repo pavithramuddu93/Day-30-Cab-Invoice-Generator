93c74a6e5014c4935d8bb7bc945a91f0974a3fbf
pavithramuddu93 <pavithramuddu93>
2022-07-05 22:19:49 +0530

    UC1 added

---
+++ src/main/java/blz/CabInvoiceGenerator.java
@@ -0,0 +1,17 @@
+package blz;
+
+public class CabInvoiceGenerator {
+
+    private final int costPerKm = 10;
+    private final int costPerMinute = 1;
+    public int minFare = 5;
+    double totalFare;
+
+    public double CalculateFare(double DISTANCE, int TIME) {
+
+        totalFare = costPerKm * DISTANCE + costPerMinute * TIME;
+        if (totalFare < minFare)
+            totalFare = minFare;
+        return totalFare;
+    }
+}
---
+++ src/test/java/blz/CabInvoiceGeneratorTest.java
@@ -0,0 +1,21 @@
+package blz;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CabInvoiceGeneratorTest {
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double TotalFare = cabInvoiceGenerator.CalculateFare(5.0, 4);
+        Assert.assertEquals(54, TotalFare, TotalFare);
+    }
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnMinFare() {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double TotalFare = cabInvoiceGenerator.CalculateFare(0.1, 2);
+        Assert.assertEquals(5, TotalFare, TotalFare);
+    }
+
+}
